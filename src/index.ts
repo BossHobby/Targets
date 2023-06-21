22bef72a4a27d7d3d16e20afaca30e27ab59a406
bkleiner <b.kleiner@str8labs.com>
2023-06-21 17:33:11 +0200

    index: sort before generating ini

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ const manufacturers = YAML.parse(
   await fs.promises.readFile("manufacturers.yaml", "utf8")
 );
 
-let targetIni = "";
 let targetIndex = [] as any[];
 
 await fs.promises.rm(OUTPUT_FOLDER, { recursive: true }).catch(() => {});
@@ -29,13 +28,6 @@ for await (const f of walk("targets")) {
   }
 
   console.log(`processing ${target.manufacturer} / ${target.name}...`);
-  if (target.manufacturer) {
-    const mgfr = target.manufacturer.toLowerCase();
-    targetIni += `\n[env:${mgfr}-${target.name}]\n`;
-  } else {
-    targetIni += `\n[env:${target.name}]\n`;
-  }
-  targetIni += `extends = ${target.mcu}\n`;
 
   targetIndex.push({
     name: target.name,
@@ -74,13 +66,23 @@ for await (const f of walk("targets")) {
 
 targetIndex.sort((a, b) => a.name.localeCompare(b.name));
 
-const index = {
-  manufacturers,
-  targets: targetIndex,
-};
-
-await fs.promises.writeFile(path.join(OUTPUT_FOLDER, "_index.ini"), targetIni);
 await fs.promises.writeFile(
   path.join(OUTPUT_FOLDER, "_index.json"),
-  JSON.stringify(index)
+  JSON.stringify({
+    manufacturers,
+    targets: targetIndex,
+  })
 );
+
+let targetIni = "";
+for (const target of targetIndex) {
+  if (target.manufacturer) {
+    const mgfr = target.manufacturer.toLowerCase();
+    targetIni += `\n[env:${mgfr}-${target.name}]\n`;
+  } else {
+    targetIni += `\n[env:${target.name}]\n`;
+  }
+  targetIni += `extends = ${target.mcu}\n`;
+}
+
+await fs.promises.writeFile(path.join(OUTPUT_FOLDER, "_index.ini"), targetIni);
